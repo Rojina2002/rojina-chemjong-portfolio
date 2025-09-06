ee0cff69f62e97f43f2d15dd0cc2e98b6df46a6c
Rojina2002 <rojina.chj@gmail.com>
2025-09-06 14:48:43 +0300

    Footer

--- src/sections/Footer/Footer.jsx
+++ src/sections/Footer/Footer.jsx
@@ -1,12 +1,24 @@
 import styles from './FooterStyles.module.css';
+import footer_logo from '../../assets/logo.png';
 
 function Footer() {
   return (
     <section id="footer" className={styles.container}>
-      <p>
-        &copy; 2024 Harris johnsen. <br />
-        All rights reserved.
-      </p>
+      <hr />
+
+      <div className={styles.footerTop}>
+        {/* Logo */}
+        <div className={styles.footerTopLeft}>
+          <img src={footer_logo} alt="Logo" />
+        </div>
+      </div>
+
+      {/* Bottom Section */}
+      <div className={styles.footerBottom}>
+        <p>
+          &copy; 2025. Rojina Chemjong — rojina.chj@gmail.com - +1 (416)-995-0371 
+        </p>
+      </div>
     </section>
   );
 }
