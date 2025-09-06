718f331a8f1409ce8651471d803fbc6ae29b578c
Rojina2002 <rojina.chj@gmail.com>
2025-09-06 14:36:32 +0300

    Hero & Projects

--- src/sections/Projects/Projects.jsx
+++ src/sections/Projects/Projects.jsx
@@ -1,7 +1,7 @@
 import styles from './ProjectsStyles.module.css';
-import viberr from '../../assets/viberr.png';
-import freshBurger from '../../assets/fresh-burger.png';
-import hipsster from '../../assets/hipsster.png';
+import weather from '../../assets/weather.png';
+import quizwhiz from '../../assets/quizwhiz.png';
+import aiva from '../../assets/aiva.png';
 import fitLift from '../../assets/fitlift.png';
 import ProjectCard from '../../common/ProjectCard';
 
@@ -11,22 +11,22 @@ function Projects() {
       <h1 className="sectionTitle">Projects</h1>
       <div className={styles.projectsContainer}>
         <ProjectCard
-          src={viberr}
-          link="https://github.com/Ade-mir/company-landing-page-2"
-          h3="Viberr"
-          p="Streaming App"
+          src={weather}
+          link="https://rchemjong-weather-app.netlify.app/"
+          h3="Weather Whiz"
+          p="Weather App"
         />
         <ProjectCard
-          src={freshBurger}
-          link="https://github.com/Ade-mir/company-landing-page-2"
-          h3="Fresh Burger"
-          p="Hamburger Restaurant"
+          src={quizwhiz}
+          link="https://quiz-whiz-rc.netlify.app/"
+          h3="Quiz Whiz"
+          p="Trivia App"
         />
         <ProjectCard
-          src={hipsster}
+          src={aiva}
           link="https://github.com/Ade-mir/company-landing-page-2"
-          h3="Hipsster"
-          p="Glasses Shop"
+          h3="AIVA"
+          p="Smart Voice Assistant"
         />
         <ProjectCard
           src={fitLift}
--- src/sections/Skills/Skills.jsx
+++ src/sections/Skills/Skills.jsx
@@ -11,27 +11,36 @@ function Skills() {
   return (
     <section id="skills" className={styles.container}>
       <h1 className="sectionTitle">Skills</h1>
+
+      {/* Core Web & Programming */}
       <div className={styles.skillList}>
         <SkillList src={checkMarkIcon} skill="HTML" />
         <SkillList src={checkMarkIcon} skill="CSS" />
         <SkillList src={checkMarkIcon} skill="JavaScript" />
-        <SkillList src={checkMarkIcon} skill="TypeScript" />
-        <SkillList src={checkMarkIcon} skill="Node" />
+        <SkillList src={checkMarkIcon} skill="JS Frameworks" />
+        <SkillList src={checkMarkIcon} skill="ReactJS" />
       </div>
+
       <hr />
+
+      {/* Backend & Databases */}
       <div className={styles.skillList}>
-        <SkillList src={checkMarkIcon} skill="React" />
-        <SkillList src={checkMarkIcon} skill="Angular" />
-        <SkillList src={checkMarkIcon} skill="Vue" />
-        <SkillList src={checkMarkIcon} skill="Tailwind CSS" />
+        <SkillList src={checkMarkIcon} skill="Node.js" />
+        <SkillList src={checkMarkIcon} skill="Express" />
+        <SkillList src={checkMarkIcon} skill="RESTful APIs" />
+        <SkillList src={checkMarkIcon} skill="SQL" />
+        <SkillList src={checkMarkIcon} skill="MongoDB" />
+        <SkillList src={checkMarkIcon} skill="Python" />
+        <SkillList src={checkMarkIcon} skill="C#" />
       </div>
+
       <hr />
+
+      {/* Tools & Methodologies */}
       <div className={styles.skillList}>
-        <SkillList src={checkMarkIcon} skill="Redux" />
-        <SkillList src={checkMarkIcon} skill="Webpack" />
         <SkillList src={checkMarkIcon} skill="Git" />
-        <SkillList src={checkMarkIcon} skill="Jest" />
-        <SkillList src={checkMarkIcon} skill="Bootstrap" />
+        <SkillList src={checkMarkIcon} skill="Figma" />
+        <SkillList src={checkMarkIcon} skill="Agile" />
       </div>
     </section>
   );
